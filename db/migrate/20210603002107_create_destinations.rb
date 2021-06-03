d6033105c573f0d14c5391787e43898ff06a3780
Wurdsmith <wurdsmithing@gmail.com>
2021-06-02 19:44:28 -0500

    Adds initial seed data to the backend.

--- db/migrate/20210603002107_create_destinations.rb
+++ db/migrate/20210603002107_create_destinations.rb
@@ -2,11 +2,12 @@ class CreateDestinations < ActiveRecord::Migration[6.1]
   def change
     create_table :destinations do |t|
       t.string :name
-      t.string :distance
-      t.string :gravity
-      t.string :temperature
-      t.string :type
+      t.integer :distance
+      t.integer :gravity
+      t.integer :temperature
+      t.string :object_type
       t.string :image_url
+      #t.belongs_to :itinerary, null: false, foreign_key: true
 
       t.timestamps
     end
